146cf1dd79db3db4159cde2040892fa622245bca
rosnajoby <rosnajoby1@gmail.com>
2024-06-03 15:56:07 +0530

    Your commit message

---
+++ string.py
@@ -0,0 +1,8 @@
+# Take input from the user
+user_string = input("Enter a string: ")
+
+# Calculate the length of the string
+length = len(user_string)
+
+# Print the length
+print("The length of the string is:", length)
